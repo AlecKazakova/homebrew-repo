8bf7d81e3b9cd99e26df80d9f523f41968e5e410
Alec Strong <astrong@squareup.com>
2020-06-26 18:02:10 -0400

    Use correct SHA

--- Formula/conway.rb
+++ Formula/conway.rb
@@ -2,7 +2,7 @@ class Conway < Formula
   desc "Print statistics on module contribution"
   homepage "https://github.com/alecstrong/conway"
   url "https://github.com/alecstrong/conway/releases/download/0.1.0/conway"
-  sha256 "f2e35f77bde11a2b5f5c867a95492d5733a2ecbba5caa6aac28c65ad1f10289e"
+  sha256 "190b35bebf12009bc55b1a0471d0fadff6ad43cfa7aff361c5771d0886e524bf"
   head "https://github.com/alecstrong/conway.git"
 
   bottle :unneeded
