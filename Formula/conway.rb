2ba1cf69de064cff4623e880eaa0a3e214c1dbe7
Alec Strong <astrong@squareup.com>
2020-06-29 18:20:26 -0400

    New version of conway

--- Formula/conway.rb
+++ Formula/conway.rb
@@ -1,8 +1,8 @@
 class Conway < Formula
   desc "Print statistics on module contribution"
   homepage "https://github.com/alecstrong/conway"
-  url "https://github.com/alecstrong/conway/releases/download/0.2.0/conway"
-  sha256 "68b05d23116feaf99a733258a3ce0a4512ba44fb09a60d38677a40dd82e12915"
+  url "https://github.com/alecstrong/conway/releases/download/0.3.0/conway"
+  sha256 "56fa6b692e108c24e7b145cb2a59dfae11972e56d44b687de6a2accfa7705374"
   head "https://github.com/alecstrong/conway.git"
 
   bottle :unneeded
